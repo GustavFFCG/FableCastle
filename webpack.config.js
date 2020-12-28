3d55bc672311e0868c42269715ba22560f0d4ab6
Gustav Öhman <43333499+GustavFFCG@users.noreply.github.com>
2020-12-28 15:10:29 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@
 
 // Dependencies. Also required: core-js, fable-loader, fable-compiler, @babel/core,
 // @babel/preset-env, babel-loader, sass, sass-loader, css-loader, style-loader, file-loader
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 var path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
